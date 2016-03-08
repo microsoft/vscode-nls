0d8639d7666aeab5240aa564ce71c66ffae0785d
Dirk Baeumer <dirkb@microsoft.com>
2016-03-08 19:25:16 +0100

    Fix broken tests

--- src/main.ts
+++ src/main.ts
@@ -71,7 +71,7 @@ function format(message: string, args: any[]): string {
 function createScopedLocalizeFunction(messages: string[]): LocalizeFunc {
 	return function(key: any, message: string, ...args: any[]): string {
 		if (isNumber(key)) {
-			if (key >= message.length) {
+			if (key >= messages.length) {
 				console.error(`Broken localize call found. Index out of bounds. Stacktrace is\n: ${(<any>new Error('')).stack}`);
 				return;
 			}
